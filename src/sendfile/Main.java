a8ba20b51ab97849855cfed707bdba4b8cdde228
Trần Thị Thanh Tâm <49787312+shyntt@users.noreply.github.com>
2020-12-27 11:57:13 +0700

    bé Tâm fix làmshowComposeScreen
    showComposeScreen()

--- src/sendfile/Main.java
+++ src/sendfile/Main.java
@@ -1,6 +1,7 @@
 package sendfile;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
--- src/sendfile/mainController.java
+++ src/sendfile/mainController.java
@@ -3,17 +3,30 @@ package sendfile;
 import com.sun.prism.Image;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.effect.MotionBlur;
+import javafx.scene.effect.Shadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.scene.image.*;
 import javax.mail.*;
+import java.awt.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
+
+import javafx.scene.paint.Color;
+import sendfile.sendMessageController;
+
+import static javafx.scene.paint.Color.GRAY;
 
 public class mainController {
 
@@ -28,33 +41,19 @@ public class mainController {
 
     @FXML
     private VBox messageListViewParent;
-    
     @FXML
     public ImageView imgCompose;
+    @FXML
+    public Button composeButton;
+    public AnchorPane showCompose;
+    @FXML
+    public VBox manageMessage;
+    @FXML
+    public ScrollPane manageScrollPane;
 
-    public static void receiveListMessages()throws MessagingException, IOException{
-        Store store = null;
-        Folder folder = null;
-        try{
-            Properties props = new Properties();
-            props.setProperty("mail.store.protocol", "imaps");
-            Session session = Session.getDefaultInstance(props, null);
-            store = session.getStore("imaps");
-            store.connect("imap.gmail.com", MailConfig.APP_EMAIL, MailConfig.APP_PASSWORD);
-            folder = store.getFolder("INBOX");
-            folder.open(Folder.READ_WRITE);
-            Message[] message = folder.getMessages();
-            ListView<String> listMessages = new ListView<String>();
-            ObservableList<String> data = FXCollections.observableArrayList();
-            for (Message value : message) {
-                data.addAll(value.toString());
-            }
-//            messageListViewParent.getChildren().addAll(listMessages);
-            VBox.setVgrow(listMessages, Priority.ALWAYS);
-
-            listMessages.setItems(data);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+    public void showComposeScreen() throws IOException {
+        FXMLLoader fXMLLoader;
+        Parent root = FXMLLoader.load(this.getClass().getResource("sendMessage.fxml"));
+        showCompose.getChildren().add(root);
     }
 }
