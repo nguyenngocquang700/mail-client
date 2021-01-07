10964bdabf3961288e3244d011f931e6ef1371da
Grizzz <40698005+nguyenngocquang700@users.noreply.github.com>
2021-01-07 19:22:32 +0700

    change temp
    change temp

--- src/sendfile/RecvConfig.java
+++ src/sendfile/RecvConfig.java
@@ -1,8 +1,8 @@
-package sendfile;
-
-public class RecvConfig {
-    public String host="pop.gmail.com";
-    public String mailStoreType="pop3";
-    String username="testmailnhom31@gmail.com";
-    String password="sicnjfvnxczuncdn";
-}
+//package sendfile;
+//
+//public class RecvConfig {
+//    public String host="pop.gmail.com";
+//    public String mailStoreType="pop3";
+//    String username="testmailnhom31@gmail.com";
+//    String password="sicnjfvnxczuncdn";
+//}
